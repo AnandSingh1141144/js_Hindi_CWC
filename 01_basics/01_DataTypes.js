17e7709944cb3c7123016ad72dd2eb3d8e063cfc
AnandSingh1141144 <97911672+AnandSingh1141144@users.noreply.github.com>
2024-04-12 15:05:51 +0000

    datatpes

---
+++ 01_basics/01_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version 
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability  should be high
+
+console.log("Anand");
+
+// for DOC 1. MDN form mozilla , 2. EMCA write for standard
+let name = "anand"
+let age = 23;
+let isLoggedIn = false
+let state; // undefined
+let sat = null;
+
+// number = > 2 to power 53
+// bigint => used iun stocks and trading
+// String => ""
+// boolean => true/flase
+// null => standalone value
+// undefined => value not assign
+// symbol => use to find uniqueness
+/* above all datatypes are primitive data types */
+
+console.log(typeof null);        // object
+console.log(typeof "Anand");     // string
+console.log(typeof undefined);   // undefined
+console.log(typeof age);         // number
