0b7182632378f64c4165229f87e4e34d74a7c90c
AnandSingh1141144 <97911672+AnandSingh1141144@users.noreply.github.com>
2024-04-19 05:41:12 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,8 +1,8 @@
 // conversion to Number
 
  let score = undefined
- console.log(typeof score);   //M1
- console.log(typeof(score));  //M2
+ console.log(typeof score);             //M1
+ console.log(typeof(score));            //M2
 
  let valueInNumber = Number(score);
  console.log(typeof valueInNumber)
@@ -46,4 +46,52 @@ console.log(sumNumber)
 33 => string
 true => string
 */
+console.log("********");
+
+// ****************************   OPERATIONS ************************************
+/*
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+
+console.log(2*2);
+
+console.log(2/2);
+console.log(2%2);             // used in cryptography
+
+*/
+console.log(true);
+console.log(+true);           // 1
+//console.log(true+);         // error
+console.log(+"");             //0   
+
+
+// for string
+let str1 = "Anand";
+let str2 = "Singh";
+
+console.log(str1 + str2);     // M1 combined print of string
+
+let str3 = (str1 + str2);
+console.log(str1 + str2);     // M2 fro combined String
+
+// *** complex Situation in String
+
+console.log("1" + 2);
+console.log(2 + "1");          // it will treat all the values that defines their at first priority
+console.log(1 + 2 + "1");
+console.log("1" + 2 + 1);
+//console.log(3 + 4 * 5 % 3);   // Wrong method to write the code  because its create a lot of misunderstanding
+console.log((3 + 4) * 5 % 3); 
+
+
+
+
+
+
+
+
 
